40be6405910aa63f110340c0c1ae6f80f7812735
Eric Mutua <erotush77@gmail.com>
2020-09-21 17:28:40 +0300

    Modified post.sh

--- .github/actions/post.sh
+++ .github/actions/post.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -H "{\"Http_X_Hub_Signature\": \"$1\", \"Content-Type\": \"application/json\"}" -X POST http://8bc9ae8ab9e4.ngrok.io/hooks/action -d $2 
+curl -H "Http_X_Hub_Signature: $1" -X POST http://8bc9ae8ab9e4.ngrok.io/hooks/action -d "$2" 
