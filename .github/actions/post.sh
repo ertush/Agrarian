a679f6ea1f3865633fa766e7b26026e76aaef955
agrarian-iot <eric.kimani@techcloud.co.ke>
2020-09-23 16:52:18 +0300

    updated post.sh

--- .github/actions/post.sh
+++ .github/actions/post.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-curl -H "Http_X_Hub_Signature: $1" -X POST http://8bc9ae8ab9e4.ngrok.io/hooks/action -d "$2" 
+echo "sig: " $1 "\npayload: " $2
+curl -H "Http_X_Hub_Signature: $1" -X POST https://ff3dee20cd67.ngrok.io/hooks/action -d "$2" 
