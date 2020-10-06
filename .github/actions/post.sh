18d5237e0819a09d265ba8e7d29e28fe1a21a44e
Eric Mutua <erotush77@gmail.com>
2020-10-07 00:22:40 +0300

    Modified post.sh

--- .github/actions/post.sh
+++ .github/actions/post.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
+# Curl post command
+# $1 -> payload sha1 hmac
+# $2 -> remote host
+# $3 -> json payload
+ 
 curl -H "Http_X_Hub_Signature: $1" -H "Content-Type: application/json" -X POST $2 -d "$3" 
