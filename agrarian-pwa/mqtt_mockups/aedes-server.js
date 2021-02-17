98707606b3775f30a5f0aba0e940cfc95c6e5b89
eric mutua <erotush77@gmail.com>
2021-02-17 10:04:41 +0300

    Added all mqtt_mockups files

--- agrarian-pwa/mqtt_mockups/aedes-server.js
+++ agrarian-pwa/mqtt_mockups/aedes-server.js
@@ -1,7 +1,7 @@
 const aedes = require('aedes')()
 const httpServer = require('http').createServer();
 const ws = require('websocket-stream');
-const port = 9100;
+const port = process.env.PORT || 9100;
 
 ws.createServer({ server: httpServer }, aedes.handle);
 
